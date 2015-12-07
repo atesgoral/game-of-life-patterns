3eb14c22a6862fd9fd297dc9ff0457b715473ae4
Ates Goral <ates@magnetiq.com>
2015-12-07 11:06:54 -0500

    Fixed whitespace

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 var assert = require('assert'),
-	patterns = require('./index.json');
+    patterns = require('./index.json');
 
 patterns.forEach(function (pattern) {
-	console.log(pattern.name);
+    console.log(pattern.name);
 
-	assert.strictEqual(pattern.height, pattern.cells.length);
+    assert.strictEqual(pattern.height, pattern.cells.length);
 
-	pattern.cells.forEach(function (row) {
+    pattern.cells.forEach(function (row) {
+        assert.strictEqual(pattern.width, row.length);
 
-	});
+        assert.ok(row.every(function (cell) {
+            return cell >= 0 && cell <= 1;
+        }));
+    });
 });
